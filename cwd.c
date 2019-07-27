4eff475c0233523470cda58d5089e16149c8feb3
sushant8421 <53339851+sushant8421@users.noreply.github.com>
2019-07-27 11:30:06 +0530

    Update cwd.c

--- cwd.c
+++ cwd.c
@@ -3,9 +3,9 @@
 #include<stdio.h>
 
 int main(int argc , char* argv[]) {
-char *buf;
-buf=(char *)malloc(100*sizeof(char));
-getcwd(buf,100);
-printf("Current Working Directory is : %s \n",buf);
-return 0 ;
+  char *buf;
+  buf=(char *)malloc(100*sizeof(char));
+  getcwd(buf,100);
+  printf("Current Working Directory is : %s \n",buf);
+  return 0 ;
 }
